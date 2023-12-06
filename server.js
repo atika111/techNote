93bbdf83f71721de0d2609998abc734e406bda05
atika111 <86054192+atika111@users.noreply.github.com>
2023-12-06 11:50:22 +0200

    Lesson-2

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
 const path = require("path");
+const { logger } = require("./middlewares/logger");
+const errorHandler = require("./middlewares/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require('./config/corsOptions')
+
+app.use(logger);
+
+app.use(cors(corsOptions))
+
+app.use(express.json);
+
+app.use(cookieParser);
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
@@ -17,6 +30,8 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port${PORT}`);
